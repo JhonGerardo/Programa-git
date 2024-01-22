37f3f69fac04b2bd1772d272e0f903c88a51c364
JhonGerardo <133007868+JhonGerardo@users.noreply.github.com>
2024-01-22 08:17:45 -0500

    Update ControladorJuego.cs
    Añadidas funcionalidades.

--- ControladorJuego.cs
+++ ControladorJuego.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class ControladorJuego : MonoBehaviour
 {
@@ -10,89 +12,108 @@ public class ControladorJuego : MonoBehaviour
     public GameObject panelArboles;
     public GameObject panelRecursosNaturales;
     public GameObject panelClasificacion;
-   
+    public Text mensajeText;
+    public float tiempoDeMensajeVisible = 8.0f;
 
-
-    private int puntaje = 20;
+    private int puntaje = 15;
     private Elemento ultimoElementoArrastrado;
+
+    private Dictionary<string, string> mensajes = new Dictionary<string, string>();
+
+    private Coroutine mensajeCoroutine;
+
+    private void Start()
+    {
+        InicializarMensajes();
+        ActualizarPuntaje();
+    }
+
+    private void InicializarMensajes()
+    {
+        // Asocia mensajes con pares específicos
+        mensajes.Add("Abeja_Flor", "¡Acierto!Las abejas supcionan el nectar de las flores y con ella crean la miel que es su alimento, y a su vez polinizan de esta forma las flores para que produzcan sus frutos.");
+        mensajes.Add("Mono_Guayabo", "¡Acierto!Los monos se alimentan de frutos como este, y dispersan las semillas diversificando asi la fauna");
+        mensajes.Add("Gusano_Lago", "Los gusanos habitan en estos medios ");
+        mensajes.Add("Colibri_Pino", "Hacen nido");
+        mensajes.Add("Oso_Naranjas", "Oso come Naranjas");
+        mensajes.Add("Ave_Cipre",  "Hace nidos");
+        mensajes.Add("Caballo_Rio", " El caballo toma agua del Rio");
+        mensajes.Add("Siervo_Sol", "Sol calienta siervo"); 
+        
+    }
 
     public void ElementoArrastrado(Elemento elemento)
     {
         if (ultimoElementoArrastrado != null && SonRelacionados(ultimoElementoArrastrado, elemento))
         {
-            puntaje += 15; 
+            puntaje += 15;
+            string clave = $"{ultimoElementoArrastrado.nombre}_{elemento.nombre}";
+            if (mensajes.ContainsKey(clave))
+            {
+                MostrarMensaje(mensajes[clave]);
+            }
+            else
+            {
+                MostrarMensaje("¡Acierto!");
+            }
+
+            // Resto de la lógica...
         }
         else
         {
-            puntaje -= 5; 
+            puntaje -= 5;
+            // Resto de la lógica...
         }
 
         ultimoElementoArrastrado = elemento;
-
         ActualizarPuntaje();
+    }
+
+    private void MostrarMensaje(string mensaje)
+    {
+        if (mensajeCoroutine != null)
+        {
+            StopCoroutine(mensajeCoroutine);
+        }
+
+        mensajeText.text = mensaje;
+        mensajeText.gameObject.SetActive(true);
+
+        mensajeCoroutine = StartCoroutine(OcultarMensajeDespuesDeTiempo());
+    }
+
+    private IEnumerator OcultarMensajeDespuesDeTiempo()
+    {
+        yield return new WaitForSeconds(tiempoDeMensajeVisible);
+        mensajeText.gameObject.SetActive(false);
     }
 
     public void ActualizarPuntaje()
     {
+        puntaje = Mathf.Clamp(puntaje, 0, 100);
         puntajeText.text = "Puntaje: " + puntaje;
 
         if (puntaje <= 0)
         {
-            
             UnityEngine.SceneManagement.SceneManager.LoadScene("EscenaFin");
-        }
-    }
-
-    public void BotonRegresarPresionado(GameObject panelActual)
-    {
-        panelActual.SetActive(false); 
-
-       
-        DesactivarElementosEnPanel(panelActual);
-
-        panelClasificacion.SetActive(true);
-        ultimoElementoArrastrado = null;
-        puntaje -= 5;
-
-        ActualizarPuntaje();
-    }
-
-    private void DesactivarElementosEnPanel(GameObject panel)
-    {
-        Elemento[] elementosEnPanel = panel.GetComponentsInChildren<Elemento>();
-
-        foreach (Elemento elemento in elementosEnPanel)
-        {
-            elemento.gameObject.SetActive(false);
         }
     }
 
     private bool SonRelacionados(Elemento elemento1, Elemento elemento2)
     {
-       
-        if (elemento1.categoria == Elemento.TipoCategoria.Animales && elemento2.categoria == Elemento.TipoCategoria.Arboles)
+        // Lógica de relación específica entre elementos
+        // Agrega más casos según tus relaciones
+        if ((elemento1.nombre == "Abeja" && elemento2.nombre == "Flor") ||
+            (elemento1.nombre == "Mono" && elemento2.nombre == "Guayabo") ||
+            (elemento1.nombre == "Gusano" && elemento2.nombre == "Lago") ||
+            (elemento1.nombre == "Colibri" && elemento2.nombre == "Pino") ||
+            (elemento1.nombre == "Oso" && elemento2.nombre == "Naranjas") ||
+            (elemento1.nombre == "Ave" && elemento2.nombre == "Cipre") ||
+            (elemento1.nombre == "Caballo" && elemento2.nombre == "Rio") ||
+            (elemento1.nombre == "Siervo" && elemento2.nombre == "Sol"))
         {
-            
-            if ((elemento1.nombre == "Abeja" && elemento2.nombre == "Flor") ||
-                (elemento1.nombre == "Mono" && elemento2.nombre == "Guayabo") ||
-                (elemento1.nombre == "Gusano" && elemento2.nombre == "Lago") ||
-                (elemento1.nombre == "Colibri" && elemento2.nombre == "Pino") ||
-                (elemento1.nombre == "Oso" && elemento2.nombre == "Naranjas") ||
-                (elemento1.nombre == "Ave" && elemento2.nombre == "Cipre") ||
-                (elemento1.nombre == "Caballo" && elemento2.nombre == "Rio") ||
-                (elemento1.nombre == "Siervo" && elemento2.nombre == "Sol"))
-            {
-                return true;
-            }
+            return true;
         }
-        else if (elemento1.categoria == Elemento.TipoCategoria.RecursosNaturales && elemento2.categoria == Elemento.TipoCategoria.Arboles)
-        {
-            if (elemento1.nombre == "Sol" && elemento2.nombre == "Eucalipto")
-            {
-                return true;
-            }
-        }
-       
 
         return false;
     }
